8eaf7f169586bbb8c1b7ab02a168b230c3e27375
jonytangtech <919632145@qq.com>
2023-07-01 15:40:35 +0800

    7.1

---
+++ JonyMasonry/Classes/TestPrint.h
@@ -0,0 +1,20 @@
+//
+//  TestPrint.h
+//  JonyMasonry
+//
+//  Created by Jony on 2023/6/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestPrint : NSObject
+
+- (void)print;
+    
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JonyMasonry/Classes/TestPrint.m
@@ -0,0 +1,16 @@
+//
+//  TestPrint.m
+//  JonyMasonry
+//
+//  Created by Jony on 2023/6/18.
+//
+
+#import "TestPrint.h"
+
+@implementation TestPrint
+
+- (void)print{
+    NSLog(@"PrintSuccess");
+}
+
+@end
